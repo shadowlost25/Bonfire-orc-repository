f0f7e17097e6ce10c41b7ff1e2312db4d9d40959
Allan Poindexter <allanjpoindexter@outlook.com>
2020-02-21 23:40:28 -0500

    Brings Xax into the new Outback; adds a LuaScript for spawning Stone Beds

---
+++ Lua Scripts/StoneBedSpawner.lua
@@ -0,0 +1,33 @@
+--OrcBed name
+local stonebed = "OrcNameBed"
+
+--Charcter coords
+local charCoords = "0," ..  --X
+                   "0," ..  --Y
+                   "0"      --Y
+
+--Coordinates
+local xcord = 0
+local ycord = 0
+local zcord = 0
+local xyzcord = xcord .. "," .. ycord .. "," .. zcord
+
+--Rotation
+local xrot = 0
+local yrot = 0
+local zrot = 0
+local xyzrot = xrot .. "," .. yrot .. "," .. zrot
+
+
+function spawnBed() 
+    orc.consolecommand("asset World/Props/Prefabs/StoneCircleLite")
+    orc.consolecommand("batch assetnameset " .. stonebed ..";asset " .. stonebed)
+    orc.consolecommand("assetpos " .. xyzcord .. "," .. xyzrot .. ",false")
+end
+
+function removeBed()
+    --Removes the spawned bed, and send the player and NPC to a previous location.
+    orc.consolecommand("assetclear " .. stonebed)
+    orc.consolecommand("batch target @self;chrpos ".. charCoords .. ";targetclear")
+    orc.consolecommand("batch target @playername;chrpos ".. charCoords .. ";nudgebwd;nudgebwd;targetclear")
+end
